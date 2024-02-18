b666d689f93586d460fcbbd8751582e22b4dfa06
hasansezertasan <hasansezertasan@gmail.com>
2024-02-18 20:25:15 +0300

    Move API Config to a config file.

---
+++ app/config.py
@@ -0,0 +1,10 @@
+API_CONFIG = {
+    "title": "Random Profile",
+    "description": "Random profile generator for designers. Name, surname, biography, username...",
+    "version": "0.0.1",
+    "contact": {
+        "name": "Hasan Sezer Taşan",
+        "url": "http://www.hasansezertasan.com",
+        "email": "hasansezertasan@gmail.com",
+    },
+}
